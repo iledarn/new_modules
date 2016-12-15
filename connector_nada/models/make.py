17da833936ea018c58fc704843f949f9ac9b1f0f
Ildar Nasyrov <nasyrov@it-projects.info>
2016-12-15 16:36:12 +0500

    [ADD] connector_nada: MakeImportMapper

--- connector_nada/models/make.py
+++ connector_nada/models/make.py
@@ -1,17 +1,25 @@
 # -*- coding: utf-8 -*-
 
 from odoo import fields, models, api
+from odoo.addons.connector.unit.mapper import ExportMapChild, ImportMapChild, ImportMapper, mapping
 
 
 class ConnectorNadaMake(models.Model):
     _name = 'connector_nada.nada.make'
     _inherit = 'nada.binding'
-    _inherits = {'nada.make': 'odoo_id'}
+    _inherits = {'nada.make': 'odoo_make_id'}
     _description = 'Nada Connector Make'
 
-    odoo_id = fields.Many2one(
+    odoo_make_id = fields.Many2one(
         comodel_name='nada.make',
         string='Make',
         required=True,
         ondelete='restrict',
     )
+    nada_make_id = fields.Integer()
+
+
+@nada
+class MakeImportMapper(ImportMapper):
+    _model_name = ['connector_nada.nada.make']
+    _map_child_class = OdooImportMapChild
